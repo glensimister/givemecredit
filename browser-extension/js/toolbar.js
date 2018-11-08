83a50713a1bbd9c6a6192d46211bea5d6585e7a7
Glen Simister <glen_simister@yahoo.co.uk>
2018-11-08 13:07:48 +0100

    changes

--- browser-extension/js/toolbar.js
+++ browser-extension/js/toolbar.js
@@ -1,6 +1,6 @@
 var Toolbar = {
     init: function () {
-        $('body').append("<div id=\"toolbar\"><div class=\"grid-toolbar\">\
+        $('body').append("<div id=\"extension_9345962\"><div id=\"toolbar\"><div class=\"grid-toolbar\">\
                         <div class=\"red\"><a><i class=\"fa fa-thumbs-o-up margin-r-5\"></i></a></div>\
                         <div>90</div>\
                         <div class=\"blue\"><a><i class=\"fa fa-thumbs-o-down margin-r-5\"></i></a></div>\
@@ -18,6 +18,6 @@ var Toolbar = {
                         </div>\
                         <div><input type=\"number\" placeholder=\"1\"></div>\
                         <div class=\"red\"><a><i class=\"fa fa-heart\"></i></a></div>\
-                    </div></div>");
+                    </div></div></div>");
     }
 }
