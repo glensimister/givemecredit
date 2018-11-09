e66474450924b0483207d0658b21bcbe15d6af47
Glen Simister <glen_simister@yahoo.co.uk>
2018-11-09 16:20:21 +0100

    changes

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ $(document).ready(function () {
         }
     });
 
-    $('.sidebar ul li.treeview a:first-child').click(function (e) {
+    $('.sidebar ul li.treeview a').click(function (e) {
 
         e.stopImmediatePropagation();
         $(this).toggleClass("open").next().slideToggle('fast');
