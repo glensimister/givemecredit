624fb5944456508a6435369ab7aaeb1e85c62f21
Glen Simister <glen_simister@yahoo.co.uk>
2018-11-26 12:22:39 +0100

    changes

--- js/fns.gun.js
+++ js/fns.gun.js
@@ -122,16 +122,16 @@ var gunAPI = {
         var count = 0;
         gun.get('elected').map().on(function (data) {
             count++;
-            let candidateSummary = `<div class="official${count}">
+            let candidateSummary = `<div class="official-${count}">
                 <div class="rateYo"></div>
                 <h4><a href="#/profile">${data.name}</a></h4>
                 <p class="position">${data.position}</p>
                 <img src="${data.photo}" class="user-image-large" alt="User Image">
                 <p>Approval rating: <b>${data.rating}</b></p>
                 <div class="grid-votes">
-                    <div class="red"><i class="fa fa-thumbs-o-up"></i></div>
+                    <div class="red"><i title="${data.id}" id="up-${count}" class="fa fa-thumbs-o-up"></i></div>
                     <div>${data.upVotes}</div>
-                    <div class="blue"><i class="fa fa-thumbs-o-down"></i></div>
+                    <div class="blue"><i title="${data.id}" id="down-${count}" class="fa fa-thumbs-o-down"></i></div>
                     <div>${data.downVotes}</div>
                 </div>
             </div>`;
