7ae6619c0a9f5a611c01010844bbeeac21fc6310
Glen Simister <glen_simister@yahoo.co.uk>
2019-01-15 13:50:22 +0100

    commit

--- js/components/displayPubService.js
+++ js/components/displayPubService.js
@@ -10,13 +10,14 @@ export async function displayPubService() {
         console.log("There are no services");
     } else {
         items.forEach(async(item) => {
-            var pOfTarget = item.value.percentageOfTarget;
-            var percentage = item.value.percentageOfTarget.split('%');
-            percentage = parseInt(percentage[0]);
-            if (percentage > 100) {
-                pOfTarget = '100%';
-            }
-            var publicService = `<div id="${item.key}">
+            if (item.value.elected) {
+                var pOfTarget = item.value.percentageOfTarget;
+                var percentage = item.value.percentageOfTarget.split('%');
+                percentage = parseInt(percentage[0]);
+                if (percentage > 100) {
+                    pOfTarget = '100%';
+                }
+                var publicService = `<div id="${item.key}">
                 <h3><a href="#/profile?id=${item.key}">${item.value.position}</a></h3>
                 <ul class="funds-raised">
                     <li>
@@ -49,55 +50,8 @@ export async function displayPubService() {
                     <div><button type="button" title="${item.key}" id="${item.key}" class="donate-sc">DONATE</button></div>
                 </div>
             </div>`;
-            $('.localServices').prepend(publicService);
+                $('.localServices').prepend(publicService);
+            }
         });
     }
-
-
-
-  /*  gun.get('services').map().once(function (data, key) {
-        if (data.isElected === true) {
-            //will move the lines below into separate file
-            var pOfTarget = data.percentageOfTarget;
-            var percentage = data.percentageOfTarget.split('%');
-            percentage = parseInt(percentage[0]);
-            if (percentage > 100) {
-                pOfTarget = '100%';
-            }
-            var publicService = `<div id="${key}">
-                <h3><a href="#/profile?id=${data.id}">${data.service}</a></h3>
-                <ul class="funds-raised">
-                    <li>
-                        Official<b class="pull-right"><a href="#/profile?id=${data.id}">${data.owner}</a></b>
-                    </li>
-                    <li>
-                        Monthly target<b class="pull-right"><span class="monthlyTarget">200</span> IDX</b>
-                    </li>
-                    <li>
-                        Raised so far<b class="pull-right"><span class="creditsReceived">${data.creditsReceived}</span> IDX</b>
-                    </li>
-                </ul>
-                <div class="progress-bar">
-                    <div style="width: ${pOfTarget};">
-                    </div>
-                </div>
-                <select class="form-control">
-                    <option>Social Credits</option>
-                    <option>Health Credits</option>
-                    <option>Education Credits</option>
-                    <option>C20 - Crypto20 (Index Fund)</option>
-                    <option>BTC - Bitcoin</option>
-                    <option>ETH - Ethereum</option>
-                    <option>LTC - Litecoin</option>
-                    <option>DASH - Dash</option>
-                </select>
-                <br />
-                <div class="grid-input-button">
-                    <div><input type="text" class="form-control donate-input"></div>
-                    <div><button type="button" title="${data.id}" id="${key}" class="donate-sc">DONATE</button></div>
-                </div>
-            </div>`;
-            $('.localServices').prepend(publicService);
-        }
-    }); */
 }
