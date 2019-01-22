100d05cfee69b0b3db31cf023c416428a498b1ad
Glen Simister <glen_simister@yahoo.co.uk>
2019-01-22 19:28:42 +0100

    commit

--- SAFESearch/safesearch.js
+++ SAFESearch/safesearch.js
@@ -1,8 +1,23 @@
 $(document).ready(function () {
     $('.fa-commenting').on('click', function () {
         $('#commentsBox').dialog({
+            title: "Comments",
             draggable: true,
             width: "auto"
         });
-    })
+    });
+
+    $('.user-image-small').on('click', function () {
+        $('.profile-summary').dialog({
+            title: "Author",
+            draggable: false,
+            width: "300px"
+        });
+        $(".rateYo").rateYo({
+            rating: 3.6,
+            fullStar: true,
+            starWidth: "20px",
+            readOnly: true
+        });
+    });
 });
